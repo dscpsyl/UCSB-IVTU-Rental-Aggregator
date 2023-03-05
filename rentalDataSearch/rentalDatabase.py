9b267e61436120a30a09a8774c0f021191b7d2f6
gento <41165204+dscpsyl@users.noreply.github.com>
2023-03-05 12:20:54 -0800

    CHANGE: Created defaults of class

--- rentalDataSearch/rentalDatabase.py
+++ rentalDataSearch/rentalDatabase.py
@@ -8,14 +8,14 @@ class RentHistoryData(DatabaseConnectionMongoDB):
         see _utils.DatabaseConnectionMongoDB for args.
     
     """
-    def __init__(self, user: str, pw: str, server: str) -> None:
+    def __init__(self, user: str, pw: str, server: str, dataBase: str) -> None:
         """Creates the instance of the class connected to the rental-data database.
 
         Args:
             See _utils.DatabaseConnectionMongoDB for args.
         """
         super().__init__(user, pw, server)
-        super().setDatabase("rental-data")
+        super().setDatabase(dataBase)
         
     def company(sef, company: str) -> None:
         """Sets the collection to the company name. This is used to get the data for a specific company.
