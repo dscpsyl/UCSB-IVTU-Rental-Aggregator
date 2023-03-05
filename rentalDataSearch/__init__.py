3c8eb67a3d5c4caec395091de80c15a41c7d3793
gento <41165204+dscpsyl@users.noreply.github.com>
2023-03-05 11:04:50 -0800

    CHANGE: Removed comments

--- rentalDataSearch/__init__.py
+++ rentalDataSearch/__init__.py
@@ -1,6 +1,4 @@
 # This module has a class in each file for each specific property. If a datapoint cannot be found, it will be set to -1. 
-# For each list of strings in the returned list (aka data of each class instance), the list has the following format:
-# Rental Company | Address | Bed | Bath | Tenants | Rent | Date Scanned
 # More infromation in each class file.
 
 __all__ = ["beachtownrentals", "rentalDatabase"]
