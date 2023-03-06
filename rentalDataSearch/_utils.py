8ccb0a2cf6ade8f6b8ce1779fe5f5dd65ce0d4eb
gento <41165204+dscpsyl@users.noreply.github.com>
2023-03-05 18:37:06 -0800

    ADD: DB methods to both classes for document handeling

--- rentalDataSearch/_utils.py
+++ rentalDataSearch/_utils.py
@@ -17,24 +17,8 @@ class RentalCompany:
         """
         
         self.data = []
-    
-    def setData(self, data: list) -> None:
-        """ Gives the user the ability to set the data of the instance. It will check that the data format has
-        the correct shape and nothing else. USE WITH CAUTION.
-        
-        args:
-            data (list(list(str))): The raw data of properties that needs to be stored.
-            
-        raises:
-            InvalidDataReceived: Will raise if the data is not in the correct format.
-
-        """    
-        
-        for r in data:
-            if len(r) != 7:
-                raise InvalidDataShapeReceived(r)
-        
-        self.data = data
+        self.__propertyData = []
+        self.__propertyRentHistory = []
     
     def __repr__(self) -> str:
         """ Returns the raw data of the instance.
@@ -62,7 +46,59 @@ class RentalCompany:
         d = pd.DataFrame(self.data, columns=_c).set_index(pd.Index(_i))
 
         return str(d)
+    
+    def setData(self, data: list) -> None:
+        """ Gives the user the ability to set the data of the instance. It will check that the data format has
+        the correct shape and nothing else. USE WITH CAUTION.
+        
+        args:
+            data (list(list(str))): The raw data of properties that needs to be stored.
+            
+        raises:
+            InvalidDataReceived: Will raise if the data is not in the correct format.
 
+        """    
+        
+        for r in data:
+            if len(r) != 7:
+                raise InvalidDataShapeReceived(r)
+        
+        self.data = data
+
+    def _dataToDocFormat(self) -> None:
+        """ Function that converts self.data and splits it into two lists with an entry for each property:
+            - self.__propertyData: A list of documents that contain the property data.
+            - self.__propertyRentHistory: A list of documents that contain the rent history data.
+        """
+        for r in self.data:
+            _doc = {
+                "address": r[1],
+                "beds": r[2],
+                "baths": r[3],
+                "tenants": r[4],
+            }
+            self.__propertyData.append(_doc)
+            
+            _doc = {
+                "address": r[1],
+                "history" : {
+                    r[6] : r[5]
+                }
+            }
+            
+            self.__propertyRentHistory.append(_doc)
+    
+    def getDBData(self) -> list:
+        """Returns the data in the database format. It will return as propertyData, propertyRentHistory.
+
+        Args:
+            type (str): The type of data to return. Can be "propertyData" or "propertyRentHistory".
+
+        Returns:
+            list: the list containing the documents
+        """
+        return self.__propertyData, self.__propertyRentHistory
+        
 class DatabaseConnectionMongoDB:
     """This is the base class for all database connections. It provides basic functions for database connection, interaction,
     and extractions. It works only with a mongoDB and assumes you are using SCRAM auth with connection link connection method. It is 
@@ -86,7 +122,7 @@ class DatabaseConnectionMongoDB:
         self.__database = None
         self.__collection = None
   
-    def newConnection(self, user: str, pw: str, server: str) -> None:
+    def _newConnection(self, user: str, pw: str, server: str) -> None:
         """Creates a new connection to a mongoDB database using SCRAM and connection string.
 
         Args:
@@ -97,7 +133,7 @@ class DatabaseConnectionMongoDB:
         _clientURL = "mongodb+srv://" + user + ":" + pw + "@" + server + "/?retryWrites=true&w=majority"
         self.__client = pm.MongoClient(_clientURL)
     
-    def setDatabase(self, db: str) -> None:
+    def _setDatabase(self, db: str) -> None:
         """Sets the desired db of the class instance. Safety checks to make sure that the databse exists.
         
         Args:
@@ -114,16 +150,16 @@ class DatabaseConnectionMongoDB:
         
         self.__database = self.__client[db]
     
-        def databasesList(self) -> list:
-            """Returns a list of all the databases in the mongoDB server.
-            
-            Returns:
-                (list(str)): A list of all the databases in the mongoDB server.
-            """
-            
-            return self.__client.list_database_names()
+    def _databasesList(self) -> list:
+        """Returns a list of all the databases in the mongoDB server.
+        
+        Returns:
+            (list(str)): A list of all the databases in the mongoDB server.
+        """
+        
+        return self.__client.list_database_names()
     
-    def listDatabases(self) -> list:
+    def _listDatabases(self) -> list:
         """Returns a list of all the databases in the mongoDB server.
         
         Returns:
@@ -132,7 +168,7 @@ class DatabaseConnectionMongoDB:
         
         return self.__client.list_database_names()
         
-    def setCollection(self, collection: str) -> None:
+    def _setCollection(self, collection: str) -> None:
         """Sets the desired collection of the class instance. Safety checks to make sure that the collection exists.
         
         Args:
@@ -148,7 +184,7 @@ class DatabaseConnectionMongoDB:
         
         self.__collection = self.__database[collection]
     
-    def listCollections(self) -> list:
+    def _listCollections(self) -> list:
         """Returns a list of all the collections in the mongoDB server.
         
         Returns:
@@ -157,16 +193,19 @@ class DatabaseConnectionMongoDB:
         
         return self.__database.list_collection_names()
     
-    def createCollection(self, collection: str) -> None:
-        """Creates a new collection in the mongoDB server.
+    def _createCollection(self, collection: str) -> None:
+        """Creates a new collection in the mongoDB server. Collection is not created until data is given.
+        Thus, we will add data to it and then delete it.
         
         Args:
             collection (str): The name of the collection to be created.
         """
         
         self.__collection = self.__database[collection]
-    
-    def getCollection(self) -> pm.collection:
+        self.__collection.insert_one({"_id": 0, "data": "data"})
+        self.__collection.delete_one({"_id": 0})
+        
+    def _getCollection(self) -> pm.collection:
         """Returns the collection of the class instance.
         
         Returns:
@@ -174,3 +213,45 @@ class DatabaseConnectionMongoDB:
         """
         
         return self.__collection
+    
+    def _listDocuments(self) -> list:
+        """Returns a list of all the documents in the collection.
+        
+        Returns:
+            (list(dict)): A list of all the documents in the collection.
+        """
+        
+        return list(self.__collection.find())
+    
+    def _getOneDocument(self, search: dict) -> dict | None:
+        """Returns a document from the collection based on a search query.
+        
+        Args:
+            search (dict): The search query to find the document.
+            
+        Returns:
+            (dict): The document from the collection. Will return None if no document is found.
+        """
+        
+        return self.__collection.find_one(search)
+    
+    def _getMatchingDocuments(self, search: dict) -> list | None:
+        """Returns a list of documents from the collection based on a search query.
+        
+        Args:
+            search (dict): The search query to find the documents.
+            
+        Returns:
+            (list(dict)): The documents from the collection. Will return None if no documents are found.
+        """
+        
+        return list(self.__collection.find(search))
+    
+    def _setOneDocument(self, document: dict) -> None:
+        """Sets a single document in the collection.
+        
+        Args:
+            document (dict): The document to be set.
+        """
+        
+        self.__collection.insert_one(document)
