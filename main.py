556544d57c16e48707afb6946ea5c858d4d5c330
gento <41165204+dscpsyl@users.noreply.github.com>
2023-03-05 21:55:12 -0800

    ADD: Finished Beach Town Rentals data aggregation

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 # A sample main file for the rentalDataSearch package. Main program is still in development.
 from rentalDataSearch import *
+import json as js
 
+print("Creating new DB connection...")
+settings = js.loads(open('env/cred.json').read())
+rDataDB = rentalDatabase.RentHistoryData(settings["mongoDBUser"], settings["mongoDBPassword"], settings["mongoDBHost"], "rental-data")
+rHistoryDB = rentalDatabase.RentHistoryData(settings["mongoDBUser"], settings["mongoDBPassword"], settings["mongoDBHost"], "rental-history")
+
+#####################~[Beach Town Rentals]~#####################
 print("Getting data from Beach Town Rentals...")
-btr = beachtownrentals.Beachtownrentals()
-btr.dataUpdate
-print(btr.dataGetPretty())
+btr = rentalCompany.BeachTownRentals()
+btr.updateData()
 print("#"*100)
+print(btr)
+print("#"*100)
+
+print("Setting data for Beach Town Rentals...")
+rentalPropData, rentalRentHist = btr.getDBData()
+rDataDB.setCompany("BeachTownRentals")
+rHistoryDB.setCompany("BeachTownRentals")
+rDataDB.insertListOfDataEntry(rentalPropData)
+rHistoryDB.insertListOfDataEntry(rentalRentHist)
